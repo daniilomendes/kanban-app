54f5eb670ebd0d10092ba26d06e6ddffc3f8fcfa
Danilo Mendes <danilo.mirandha@gmail.com>
2024-04-15 12:18:55 -0300

    feat: add DropArea component and styles

---
+++ src/components/DropArea.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./DropArea.css";
+
+const DropArea = ({ onDrop }) => {
+  const [showDrop, setShowDrop] = useState(false);
+  return (
+    <section
+      onDragEnter={() => setShowDrop(true)}
+      onDragLeave={() => setShowDrop(false)}
+      onDrop={() => {
+        onDrop();
+        setShowDrop(false);
+      }}
+      onDragOver={(e) => e.preventDefault()}
+      className={showDrop ? "drop_area" : "hide_drop"}
+    >
+      Solte aqui
+    </section>
+  );
+};
+
+export default DropArea;
